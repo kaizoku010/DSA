522e137b0d1b2a67a78ba7dba3cec6cab5e83eed
Muneza Dixon <dixontheworldvsy@gmail.com>
2025-01-02 03:37:54 +0300

    uodated bb sort

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,23 +1,25 @@
 function bubbleSort(arr){
+
 //sort in ascending order 
 //1,2,3,4,6,7,8...
 //if arr = 8,3,1,2,4,5,6,7
-    let swapped
-    //base case 
-    if(!arr) return null
-
+//this will determine if we need more sorting or not
+let swapped
+//basecase
+if(!arr) return null
 do{
-    swapped = false
-    for(let i = 0; i < arr.length - 1; i ++){
-        if(arr[i] > arr[i + 1]){
-            let temp = arr[i]
-            //reasign it after saving it in our temp variable
-            arr[i] = arr[i + 1]
-            arr[i +1] = temp
-            swapped = true
-        }
+swapped = false
+//O(s) linear time
+for(let i = 0; i < arr.length - 1; i++){
+    if(arr[i] > arr[i + 1]){
+        let temp = arr[i]
+        //reassgn after swapping
+        arr[i] = arr[i + 1]
+        //do the swap
+        arr[i + 1] = temp
+        swapped = true
     }
+}
 } while(swapped)
-
 
 }
